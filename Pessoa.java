8eca2c5cb756293366708fb90fdc33953119b860
Rafael Mota Alves <rma7@cin.ufpe.br>
2017-06-14 15:39:45 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,21 @@
+
+public abstract class Pessoa {
+	private String cpf;
+	private String nome;
+	public Pessoa(String cpf,String nome){
+		this.cpf=cpf;
+		this.nome=nome;
+	}
+	public String getCpf(){
+		return this.cpf;
+	}
+	public void setCpf(String cpf){
+		this.cpf=cpf;
+	}
+	public String getNome(){
+		return this.nome;
+	}
+	public void setNome(){
+		this.nome=nome;
+	}
+}
